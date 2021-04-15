8d4035a3dc40462d13a290784bd94953ec327c25
bae <baesj11@gmail.com>
2021-04-15 19:08:43 +0900

    Recursion2

--- src/main/java/Recursion/Main.java
+++ src/main/java/Recursion/Main.java
@@ -8,6 +8,7 @@ public class Main {
         int num;
         num = sc.nextInt();
         System.out.println("1부터 " + num + "까지 합 = " + numsum(num) );
+        System.out.println(num + "! = " + factorial(num));
         sc.close();
     }
     public static int numsum(int num)
@@ -19,6 +20,13 @@ public class Main {
         else
         {
             return num + numsum(num -1);
+        }
+    }
+    public static int factorial(int num){
+        if(num==1){
+            return num;
+        }else{
+            return num*factorial(num -1);
         }
     }
 }
