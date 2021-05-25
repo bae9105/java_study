df97806c47ac4291437bc3a61c3bcd6f740a1095
bae <baesj11@gmail.com>
2021-05-25 20:14:28 +0900

    Algo_0525

--- src/main/java/A4948/A4948.java
+++ src/main/java/A4948/A4948.java
@@ -1,34 +1,37 @@
 package A4948;
 
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class A4948 {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws IOException{
         Scanner sc = new Scanner(System.in);
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        boolean[] box = new boolean[123456*2+1];
+        Arrays.fill(box, true);
+        box[0] = false;
+        box[1] = false;
+        for(int i = 2;i<=Math.sqrt(123456*2);i++){
+            if(box[i]==false){
+                continue;
+            }
+            for(int j = i*i;j<=box.length-1;j+=i){
+                box[j] = false;
+            }
+        }
         while(true){
             int input = sc.nextInt();
-            int priNum = 0;
-            if(input==0){
+            int cnt = 0;
+            if(input == 0){
                 break;
             }
-            for(int i = input;i<=input*2;i++){
-                int cnt = 0;
-                for(int j = 2;j<=i;j++){
-                    if(i%j==0){
-                        cnt++;
-                    }
-                    if(cnt==2){
-                        break;
-                    }
-                }
-                if(cnt==1){
-                    priNum +=1;
+            for(int i = input+1;i<=input*2;i++){
+                if(box[i]==true){
+                    cnt++;
                 }
             }
-            bw.write(priNum+"\n");
+            bw.write(cnt+"\n");
         }
         bw.flush();
         bw.close();
